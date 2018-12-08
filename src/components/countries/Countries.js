1729ad6bbf850d3fbde9facf2d869e50ce592015
nleitefaria <nleitefaria@gmail.com>
2018-12-08 12:23:04 +0000

    Added Countries view

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -1,14 +1,127 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import { Breadcrumb, BreadcrumbItem, Card, Row, Col } from 'reactstrap';
+import { PagingState, CustomPaging} from '@devexpress/dx-react-grid';
+import { Grid, Table, TableHeaderRow, PagingPanel} from '@devexpress/dx-react-grid-bootstrap4';
+import Loading from '../Loading';
+
+const apiURL = 'https://sec-os-app4.193b.starter-ca-central-1.openshiftapps.com';
+
+var divLoading =
+{
+	'float': 'left', 'width': '300px', 'paddingTop': '0px', 'paddingLeft': '10px'
+};
 
 class Home extends Component
 {
-	render() {
-	    return (
-		<div>
-			Countries
-		</div>
-	    );
+	constructor(props)
+	{
+		super(props);
+        this.state = {
+        		results: [],
+        		columns: [
+        			{ name: 'title', title: 'Title'},
+        			{ name: 'director', title: 'Director' },
+        			{ name: 'producer', title: 'Producer' },
+        		],
+        		rows: [],
+        		totalCount: 0,
+        		currentPage: 0,
+        		loading: true,
+						modal: false
+        };
+
+        this.changeCurrentPage = this.changeCurrentPage.bind(this);
 	}
+
+	componentDidMount()
+	{
+		this.loadData();
+	}
+
+	componentDidUpdate()
+	{
+		this.loadData();
+	}
+
+	changeCurrentPage(currentPage)
+	{
+		this.setState({
+			loading: true,
+			currentPage: currentPage,
+		});
+	}
+
+	queryString()
+	{
+			const { currentPage } = this.state;
+			var cPage = Number(currentPage) + 1;
+			return apiURL.concat('/countries/').concat(cPage).concat('/10');
+	}
+
+	loadData()
+	{
+		const queryString = this.queryString();
+		if (queryString === this.lastQuery) {
+		    return;
+		}
+		/*
+		fetch(queryString)
+		      .then(response => response.json())
+		      .then(data => this.setState({
+		        rows: data.content,
+		        totalCount: data.totalElements,
+		        loading: false,
+		      }))
+		      .catch(() => this.setState({ loading: false }));
+		    	this.lastQuery = queryString;
+		 */
+		
+		axios.get(queryString).then(res =>
+	      {
+	        const rd = res.data;
+	        //this.setState( {rows: rd} );
+	        //this.setState( {loading: false} );
+
+	        
+	        this.setState({
+		        rows: rd.content,
+		        totalCount: rd.totalElements,
+		        loading: false,
+		      })
+	        
+	        
+	        
+	      })
+	 }
+	  	
+
+		render()
+		{
+			const { rows, columns, pageSize, currentPage, totalCount , loading } = this.state;
+			return (
+			      <div>
+			      	<Breadcrumb>
+		      			<BreadcrumbItem><a href="#/" rel="noopener noreferrer">Home</a></BreadcrumbItem>
+		      			<BreadcrumbItem active>Films</BreadcrumbItem>
+		      		</Breadcrumb>
+		      	  	<br></br>
+								<Row>
+                    <Col xs="6"><div style={divLoading}>{loading && <Loading />}</div></Col>
+                </Row>
+			      	<Card style={{ position: 'relative' }}>
+  	        			<Grid rows={rows} columns={columns}>
+  	        				<PagingState currentPage={currentPage} onCurrentPageChange={this.changeCurrentPage} pageSize={pageSize} />
+  	        					<CustomPaging totalCount={totalCount} />
+  	          						<Table/>
+  	          							<TableHeaderRow />
+  	          								<PagingPanel />
+  	          		</Grid>
+  	          		</Card>
+			      </div>
+			    );
+		}
 }
 
 export default Home;
